b5fce98367314fb96b14f75168328e2a32bf99eb
Rabea Turon <rabea.turon@tu-darmstadt.de>
2026-05-18 18:46:15 +0200

    add binary psychometric model

--- hope/psychometric_model.py
+++ hope/psychometric_model.py
@@ -1,50 +1,28 @@
+from abc import ABC, abstractmethod
+import math
+from typing import Dict
+
 import numpy as np
+from scipy import stats
+from scipy.stats._distn_infrastructure import rv_frozen
+from scipy.special import expit
 
 from .sequential_monte_carlo import WeightedParticles
 
 
-class PsychometricModel(ABC):
-    def __init__(
-        self, prior_params
-    ):  # TODO: talk to Swantje to check how to pass distributions the best
-        self.prior_params = prior_params
+class BinaryPsychometricModel(ABC):
+    def __init__(self, seed=None):
+        self.priors: dict[str, rv_frozen] = ...
+        self.trans_prop = None # optional if transforms for distributions should be used
+        self.seed = seed
 
     @abstractmethod
+    def psychometric_fuction(self, X, fct_params):
+        # Define this function in your own class!
+        pass
+
     def likelihood(self, X, responses, fct_params):
-        pass
-
-    @abstractmethod
-    def log_likelihood(self, X, reponses, fct_params):
-        pass
-
-    def sample_prior(self, n_samples) -> np.ndarray:
-        pass
-
-
-class LogisticRegressionWithLapses(PsychometricModel):
-    def __init__(
-        self, n_dims, priors
-    ):  # TODO: talk to Swantje to check how to pass distributions the best
-        super().__init__()
-        self.n_dims = n_dims
-        # TODO: do we want the priors here or params for the priors instead?
-        #       also: checks to see if the order fits what we expect (lapses have
-        #       other priors than other parameters). If we decide to use params here
-        #       we have to adapt the base class.
-        self.trans_prop = trans_prop  # TODO: look up what the trans_prop should be
-        self.priors = priors
-
-    @staticmethod
-    def likelihood(X, responses, fct_params):
-        if X.size == 1 and X.ndim <= 1:
-            X = X.reshape((1, 1))
-        if X.ndim == 1:
-            X = np.expand_dims(X, axis=0)
-        a = fct_params[:, 0]
-        k = fct_params[:, 1]
-        p = a + (k - a) * expit(X @ fct_params[:, 2:].T)
-        p[p > 1] = 1
-        p[p < 0] = 0 + 1e-100
+        p = self.psychometric_function(X, fct_params)
         likelihoods = np.prod(
             (np.power(p, responses) * np.power(1 - p, 1 - responses)), axis=0
         )
@@ -52,51 +30,73 @@ class LogisticRegressionWithLapses(PsychometricModel):
         likelihoods[likelihoods < 0] = 0
         return likelihoods
 
-    @staticmethod
-    def log_likelihood(X, responses, t):
-        a = t[:, 0].reshape(-1, 1)
-        k = t[:, 1].reshape(-1, 1)
-        s = expit(t[:, 2:] @ X.T)
-        p = a + (k - a) * s
-        p = np.clip(p, 0 + 1e-100, 1)
-        res = np.sum(
+    def log_likelihood(self, X, responses, fct_params):
+        p = self.psychometric_function(X, fct_params)
+        log_likelihoods = np.sum(
             np.log(np.where(responses, p, 1) * np.where(1 - responses, 1 - p, 1)),
             axis=1,
         )
-        return res
-
-    # @staticmethod
-    # def likelihood_importance_reweighting_wrapper(data, locations):
-    #     x, responses = data
-    #     x = np.hstack([1, x])
-    #     return LogisticLapseRegressionLikelihood.likelihood(x, responses, locations)
+        return log_likelihoods
 
     def sample_prior(self, n_samples) -> np.ndarray:
-        def initialize_posteriors(
-            priors, trans_prop, n_particles
-        ):  # some function that samples the first particles from priors
-            rng = np.random.default_rng(seed)
-            # sample from prior to initialize particles
-            prior_samples = []
-            only_one = True
-            for i, prior in enumerate(priors):
-                if i != len(priors) - 1:
-                    new_samples = prior.rvs(size=n_particles, random_state=seed)
-                    if trans_prop:
-                        new_samples = new_samples.clip(
-                            trans_prop.lower_bounds[i], trans_prop.upper_bounds[i]
-                        )
-                    prior_samples = prior_samples + [new_samples.tolist()]
-                    only_one = False
-                else:
-                    if only_one:
-                        prior_samples = prior.rvs(size=n_particles, random_state=seed)
-                        break
-                    prior_samples = np.array(prior_samples).T
-                    prior_samples = np.hstack(
-                        [prior_samples, prior.rvs(size=n_particles, random_state=seed)]
+        prior_samples = []
+        only_one = True
+        for i, prior in enumerate(self.priors.values()):
+            if i != len(self.priors) - 1:
+                new_samples = prior.rvs(size=n_samples, random_state=self.seed)
+                if self.trans_prop:
+                    new_samples = new_samples.clip(
+                        self.trans_prop.lower_bounds[i], self.trans_prop.upper_bounds[i]
                     )
-            # if initial_particles is None:
-            particles = WeightedParticles(prior_samples)
-            logging.info("Initialized particles from priors.")
-            return particles
+                prior_samples = prior_samples + [new_samples.tolist()]
+                only_one = False
+            else:
+                if only_one:
+                    prior_samples = prior.rvs(size=n_samples, random_state=self.seed)
+                    break
+                prior_samples = np.array(prior_samples).T
+                prior_samples = np.hstack(
+                    [prior_samples, prior.rvs(size=n_samples, random_state=self.seed)]
+                )
+        particles = WeightedParticles(prior_samples)
+        return particles
+
+
+class LogisticRegressionWithLapses(BinaryPsychometricModel):
+    def __init__(
+        self, n_dims, seed=None
+    ):  # TODO: talk to Swantje to check how to pass distributions the best
+        super().__init__()
+        self.n_dims = n_dims
+        self.trans_prop = IntervalTransformIndependentGaussianProposer(
+            [0, 1], np.array([0, 0.8]), np.array([0.2, 1])
+        )
+        prior = stats.multivariate_normal(
+            mean=np.zeros(self.n_dims), variance=3, seed=seed
+        )
+        self.priors = {
+            "lower_lapse": stats.beta(1, 30),
+            "upper_lapse": stats.beta(1, 30),
+            "weights": prior,
+            "bias": stats.norm(scale=math.sqrt(3), seed=seed),
+        }
+
+    def psychometric_function(self, X, fct_params):
+        if X.size == 1 and X.ndim <= 1:
+            X = X.reshape((1, 1))
+        if X.ndim == 1:
+            X = np.expand_dims(X, axis=0)
+        a = fct_params[:, 0]
+        k = fct_params[:, 1]
+        s = expit(fct_params[:, 2:] @ X.T)
+        p = a + (k - a) * s
+        return p
+
+    def likelihood(self, X, responses, fct_params):
+        return super().likelihood(self, X, responses, fct_params)
+
+    def log_likelihood(self, X, responses, fct_params):
+        return super().log_likelihood(self, X, responses, fct_params)
+
+    def sample_prior(self, n_samples) -> np.ndarray:
+        return super().sample_prior(self, n_samples)
