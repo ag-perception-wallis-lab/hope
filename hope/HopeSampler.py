7a7af73283b76e4373343d9cf2ff1185f43935da
Lars Reining <lars.reining@stud.tu-darmstadt.de>
2026-05-18 16:38:40 +0200

    Clean copied code

--- hope/HopeSampler.py
+++ hope/HopeSampler.py
@@ -1,127 +1,97 @@
+import numpy as np
+
+from hope.psychometric_model import PsychometricModel
+
+from .sequential_monte_carlo import (
+    WeightedParticles,
+    adapt_proposal_width_factor,
+    importance_reweighting,
+    metropolis_hastings_step,
+    metropolis_step,
+    mutual_information,
+    propose_independent_gaussian,
+    rule_of_thumb_bandwidths,
+)
+
 __all__ = ["HopeSampler"]
 
 
-class HopeSampler():
+# TODO: Think about loading previous HopeSampler
+class HopeSampler:
     def __init__(
         self,
-        psychometric_model,
-        stimulus_pool, # stimulus pool --> should we add other option with range with min and max?
-        n_particles,
-        n_mh,
-	    proposal_dist,
-        replace_after_trials=1, # filling up of stimulus pool after this amount of trials
-        seed=None,
+        psychometric_model: PsychometricModel,
+        stimulus_pool: np.ndarray,  # TODO stimulus pool --> should we add other option with range with min and max?
+        n_particles: int,
+        n_mh: int,
+        proposal_dist=None,  # TODO document default proposal distribution
+        replace_after_trials: int = 1,
+        seed=None,  # TODO implement seeding
     ):
-        # TODO: Think about loading previous HopeSampler
+        # TODO docstring
+        self.rng = np.random.default_rng(seed)
         self.psychometric_model = psychometric_model
         self.proposal_dist = proposal_dist
         self.n_mh = n_mh
-        self.stimulus_pool = stimulus_pool # all stimuli
-        self.X = stimulus_pool # current stimulus pool; might change if we sample without replacement
+        self.stimulus_pool = stimulus_pool  # all stimuli
+        self.X = stimulus_pool  # current stimulus pool; might change if we sample without replacement
         self.replace_after_trials = replace_after_trials
 
-
-        def initialize_posteriors(priors, trans_prop, n_particles):  # some function that samples the first particles from priors
-            rng = np.random.default_rng(seed)
-            # sample from prior to initialize particles
-            prior_samples = []
-            only_one = True
-            for i, prior in enumerate(priors):
-                if i != len(priors) - 1:
-                    new_samples = prior.rvs(size=n_particles, random_state=seed)
-                    if trans_prop:
-                        new_samples = new_samples.clip(
-                            trans_prop.lower_bounds[i], trans_prop.upper_bounds[i]
-                        )
-                    prior_samples = prior_samples + [new_samples.tolist()]
-                    only_one = False
-                else:
-                    if only_one:
-                        prior_samples = prior.rvs(size=n_particles, random_state=seed)
-                        break
-                    prior_samples = np.array(prior_samples).T
-                    prior_samples = np.hstack(
-                        [prior_samples, prior.rvs(size=n_particles, random_state=seed)]
-                    )
-            # if initial_particles is None:
-            particles = WeightedParticles(prior_samples)
-            logging.info("Initialized particles from priors.")
-            return particles
-
         # TODO: initial particles should not be here anymore now?
-        self.particles = initialize_posteriors(
-            psychometric_model.priors,
-            psychometric_model.trans_prop,
-            n_particles)
+        self.n_particles = n_particles
+        self.particles = WeightedParticles(
+            self.psychometric_model.sample_prior(n_particles)
+        )
         self.sampled = []
         self.responses = []
 
     def get_next_stimulus(self):
-        """Copied and deleted code from data_sampling.py PsiSampler
-
-        Returns: next stimulus 
-        """
-        # if self.X.shape[0] < self.min_n_samples:
-        #     self.generate_samples(rng)
-        # if not rng:
-        #     rng = np.random.default_rng()
-        # if self.particles is not None:
-        probs = self.likelihood(self.X, self.particles)
+        # TODO add documentation about sampling with replacement and without replacement and how the stimulus pool is handled in both cases
+        if (
+            self.stimulus_pool.shape[0] - self.X.shape[0] >= self.replace_after_trials
+        ):  # TODO catch stimulus pool empty error
+            self.X = self.stimulus_pool.copy()
+        probs = self.psychometric_model.likelihood(self.X, self.particles)
         next = np.argmax(mutual_information(probs))
-        # else:
-        #     next = rng.integers(0, self.X.shape[0])
-        x = self.X[next]
-        # response = self.responder.get_response(x)
-        
+        stimulus = self.X[next]
         self.X = np.delete(self.X, next, axis=0)
-        return x
-    
+        return stimulus
+
     def update_posterior(self, stimulus, response):
         self.sampled.append(stimulus)
         self.responses.append(response)
-
-        # now use the new information to update the posterior
         self.particles.weights = importance_reweighting(
             self.particles.locations,
             self.particles.weights,
-            [_x[1:], _res],
-            Likelihood.likelihood_importance_reweighting_wrapper,
+            [stimulus, response],
+            self.psychometric_model.likelihood_importance_reweighting_wrapper,
         )
-        self.particles.importance_resampling(method="stratified", rng=rng)
-        particle_duplicate_ratio = self.particles.duplicate_ratio
-        logging.debug("_x:", _x)
-        logging.debug("_res:", _res)
-        logging.debug(f"Effective N of self.particles: {n_eff(self.particles.weights)}")
-        logging.debug(f"Duplicate Ratio: {particle_duplicate_ratio}")
-        logging.debug("Starting Metropolis")
+        self.particles.importance_resampling(method="stratified", rng=self.rng)
 
-        def unnormalized_log_posterior(samples):
-            log_prior = 0
-            h = 0
-            for prior in priors[:-1]:
-                log_prior += prior.logpdf(samples[:, h])
-                h += 1
-            log_prior += priors[-1].logpdf(samples[:, h:])
-            ll = Likelihood.log_likelihood(np.array(X), np.array(res).T, samples)
+        def unnormalized_log_posterior(particle_locations):
+            log_prior = self.psychometric_model.log_prior(particle_locations)
+            ll = self.psychometric_model.log_likelihood(
+                np.array(self.sampled), np.array(self.responses).T, particle_locations
+            )
             return log_prior + ll
 
         j = 0
         acceptance_probs = []
-
-        # do some mcmc steps (rule to be implemented)
+        proposal_width_factor = 1.0
+        # do some mcmc steps (rule to be implemented) TODO
         while j < self.n_mh:
-            if store_run_config:
-                save()
-            if seed is not None:
-                seed = seed + j
-            if self.trans_prop is not None:
+            if self.seed is not None:
+                self.seed = self.seed + j
+            if self.psychometric_model.trans_prop is not None:
                 if j < 5:
                     proposal_vars = proposal_width_factor * rule_of_thumb_bandwidths(
-                        trans_prop.transform(self.particles.locations),
+                        self.psychometric_model.trans_prop.transform(
+                            self.particles.locations
+                        ),
                         effective_n=self.particles.n_particles
                         - self.particles.duplicate_ratio * self.particles.n_particles,
                     )
-                    trans_prop.proposal_vars = proposal_vars
+                    self.psychometric_model.trans_prop.proposal_vars = proposal_vars
                 (
                     new_locations,
                     indices_of_updated_locations,
@@ -129,8 +99,8 @@ class HopeSampler():
                 ) = metropolis_hastings_step(
                     self.particles.locations,
                     unnormalized_log_posterior_fn=unnormalized_log_posterior,
-                    trans_proposer=trans_prop,
-                    rng=rng,
+                    trans_proposer=self.psychometric_model.trans_prop,
+                    rng=self.rng,
                 )
             else:
                 if j < 5:
@@ -148,27 +118,13 @@ class HopeSampler():
                     unnormalized_log_posterior_fn=unnormalized_log_posterior,
                     proposal_fn=propose_independent_gaussian,
                     proposal_vars=proposal_vars,
-                    rng=rng,
+                    rng=self.rng,
                 )
+            # todo when are we doing mh or h steps, document properly
             acceptance_probs.append(acceptance_prob)
             self.particles.update_locations(new_locations, indices_of_updated_locations)
             j += 1
-            logging.debug(f"MH step {j}")
-            logging.debug(f"Acceptance Prob: {acceptance_prob}")
-            logging.debug(f"Duplicate Ratio: {self.particles.duplicate_ratio}")
-
-        logging.info(f"{j} metropolis (hastings) steps were made.")
-        if store_run_config:
-            save()
-
         # dynamically adapt proposal width
         proposal_width_factor = adapt_proposal_width_factor(
             proposal_width_factor, acceptance_probs
         )
-        logging.debug(f"New proposal width factor: {proposal_width_factor}")
-        if data_sampler.responder.finished:
-            if not store_particle_positions:
-                store_particle_positions = True
-                save()
-            break
-
---
+++ hope/sequential_monte_carlo/metropolis_hastings.py
@@ -0,0 +1,241 @@
+from abc import ABC, abstractmethod
+from collections.abc import Callable
+from typing import Tuple
+
+import numpy as np
+import torch
+from scipy import stats
+from torch.distributions.multivariate_normal import MultivariateNormal
+
+from .sequential_monte_carlo import n_eff
+
+
+def adapt_proposal_width_factor(
+    proposal_width_factor, acceptance_probs, acceptance_boundaries=[0.15, 0.5]
+):
+    """
+    Adapts the proposal distribution's width factor if the acceptance
+    probabilities are too small or too large.
+
+    Parameters
+    ----------
+    acceptance_probs: Iterable
+        The particle MH acceptance probabilities during the last sequence of MH
+        steps (for the most recent data point).
+    """
+    mean_acceptance = np.mean(acceptance_probs)
+    adaptor = 1.0
+    if mean_acceptance < acceptance_boundaries[0]:
+        adaptor = 0.5
+    if mean_acceptance > acceptance_boundaries[1]:
+        adaptor = 2.0
+    return adaptor * proposal_width_factor
+
+
+def rule_of_thumb_bandwidths(locations, effective_n=None, weights=None):
+    """
+    Assumes that the locations are i.i.d. samples or weighted. If they are
+    weighted one needs to compute the actual effective number of particles. A
+    problem might arise if we have weights = None or weights = 1 / N but the
+    particles are acutally duplicates of each other (which usually happens after
+    a importance resampling step). Then we will overestimate the effctive sample
+    size.
+
+    The effective sample size can be computed as we normally do it for particle
+    filters
+    (https://en.wikipedia.org/wiki/Effective_sample_size#Weighted_samples).
+
+    See
+    https://en.wikipedia.org/wiki/Kernel_density_estimation#A_rule-of-thumb_bandwidth_estimator
+    and
+    https://en.wikipedia.org/wiki/Multivariate_kernel_density_estimation#Rule_of_thumb
+    for an explanation of where these formulas come frome.
+    """
+    if effective_n is None:
+        if weights is None:
+            effective_n = locations.shape[0]
+        elif weights is not None:
+            effective_n = n_eff(weights)
+    stds = np.std(locations, axis=0)
+    iqrs = stats.iqr(locations, axis=0)
+    min = np.minimum(stds, iqrs / 1.34)
+    n_dim = locations.shape[1]
+    bandwidths = min * effective_n ** (-1 / (n_dim + 4))
+    return bandwidths
+
+
+def propose_isotropic_gaussian(samples, proposal_std=1.0, seed: int = None):
+    # numpy can't sample from a multivariate normal in a batch fashion (although
+    # in this specific case (since I'm using a diagonal covariance matrix) there
+    # would have been a workaround of staying in 1D), so I turned to torch.
+    if seed is not None:
+        torch.set_rng_state(torch.manual_seed(seed).get_state())
+    mvn = MultivariateNormal(
+        loc=torch.from_numpy(samples),
+        covariance_matrix=proposal_std**2 * torch.eye(samples.shape[1]),
+    )
+    return mvn.sample().numpy()
+
+
+def propose_independent_gaussian(
+    samples, proposal_vars=None, proposal_var_fn=None, rng: np.random.Generator = None
+):
+    assert (
+        proposal_vars is not None or proposal_var_fn is not None
+    ), "Either proposal_vars or proposal_var_fn must be passed."
+    if not rng:
+        rng = np.random.default_rng()
+    if proposal_var_fn is not None:
+        proposal_vars = proposal_var_fn(samples)
+    torch.manual_seed(rng.integers(0, 2**32 - 1))
+    mvn = MultivariateNormal(
+        loc=torch.from_numpy(samples),
+        covariance_matrix=torch.from_numpy(np.diag(proposal_vars)),
+    )
+    return mvn.sample().numpy()
+
+
+def metropolis_step(
+    samples: np.ndarray,
+    unnormalized_log_posterior_fn: Callable,
+    proposal_fn: Callable,
+    rng: np.random.Generator = None,
+    **proposal_fn_kwargs,
+) -> Tuple[np.ndarray, np.ndarray, float]:
+    """
+    Implements a standard Metropolis step with proposals and acceptance. For the
+    Metropolis algorithm it is not possible to transform the samples into a
+    different space to do proposals there since the pdf in the original space
+    will not be symmetric even if it is in the transformed space (due to the
+    Jacobian of the transformation). See e.g. here for an explanation:
+    https://barumpark.com/blog/2019/Jacobian-Adjustments/
+
+    Parameters
+    ----------
+    samples: np.ndarray
+        An array of samples with shape (n_samples, n_dimensions)
+    unnormalized_log_posterior_fn: Callable
+        Function that computes the unnormalized posterior probability density
+        given a sample with dimensionality n_dimensions
+    proposal_fn: Callable
+        Function that proposes new sample locations given the old sample
+        locations
+    rng: np.random.Generator
+        Random generator
+
+    Returns
+    -------
+    new_samples: np.ndarray
+        The locations of the accepted proposals
+    indices_of_updated_samples: np.ndarray
+        The indices of the samples that have an accepted proposal (i.e. that
+        were updated)
+    acceptance_prob: float
+        The ratio of accepted proposals
+    """
+    proposals = proposal_fn(samples, rng=rng, **proposal_fn_kwargs)
+    acceptance_probs = np.minimum(
+        np.exp(
+            unnormalized_log_posterior_fn(proposals)
+            - unnormalized_log_posterior_fn(samples)
+        ),
+        np.ones(len(samples)),
+    )
+    accept = rng.binomial(n=1, p=acceptance_probs).reshape(-1, 1)
+    indices_of_updated_samples = np.nonzero(accept.flatten())[0]
+    acceptance_prob = np.sum(accept) / len(accept)
+    new_samples = proposals[indices_of_updated_samples]
+    return new_samples, indices_of_updated_samples, acceptance_prob
+
+
+class TransformProposer(ABC):
+    @abstractmethod
+    def propose(
+        self, samples: np.ndarray, rng: np.random.Generator = None
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """Method to optionally transform the samples into a different space (e.g.
+        log-space) to make proposals there and that returns the proposals (in
+        original space) and the logpdf of the samples given the proposals and
+        the proposals given the sample (while accounting for the possible
+        transformation of variable and the resulting need to include the
+        Jacobian of the transformation).
+
+        Parameters
+        ----------
+        samples : np.ndarray
+            An array of samples with shape (n_samples, n_dimensions)
+        rng : np.random.Generator, optional
+            Random generator, by default None
+
+        Returns
+        -------
+        proposals: np.ndarray
+            An array of proposals in the original space with the same shape as
+            samples.
+        proposal_logpdf: np.ndarray
+            logpdf of the proposals given the samples (in original space; i.e.
+            possibly accounting for the change of variables).
+        sample_logpdf: np.ndarray
+            logpdf of the samples given the proposals (in original space; i.e.
+            possibly accounting for the change of variables).
+        """
+        pass
+
+
+def metropolis_hastings_step(
+    samples: np.ndarray,
+    unnormalized_log_posterior_fn: Callable,
+    trans_proposer: TransformProposer,
+    rng: np.random.Generator = None,
+) -> Tuple[np.ndarray, np.ndarray, float]:
+    """
+    Implements a standard Metropolis step with proposals and acceptance. For the
+    Metropolis algorithm it is not possible to transform the samples into a
+    different space to do proposals there since the pdf in the original space
+    will not be symmetric even if it is in the transformed space (due to the
+    Jacobian of the transformation). See e.g. here for an explanation:
+    https://barumpark.com/blog/2019/Jacobian-Adjustments/
+
+    Parameters
+    ----------
+    samples: np.ndarray
+        An array of samples with shape (n_samples, n_dimensions)
+    unnormalized_log_posterior_fn: Callable
+        Function that computes the unnormalized posterior probability density
+        given a sample with dimensionality n_dimensions
+    trans_proposer: TransformProposer
+        Transformproposer with method propose that optionally transforms the
+        samples into a different space (e.g. log-space) to make proposals there
+        and that returns the proposals (in original space) and the logpdf of the
+        samples given the proposals and the proposals given the sample (while
+        accounting for the possible transformation of variable and the resulting
+        need to include the Jacobian of the transformation).
+    rng: np.random.Generator
+        Random generator
+    Returns
+    -------
+    new_samples: np.ndarray
+        The locations of the accepted proposals
+    indices_of_updated_samples: np.ndarray
+        The indices of the samples that have an accepted proposal (i.e. that
+        were updated)
+    acceptance_prob: float
+        The ratio of accepted proposals
+    """
+    if not rng:
+        rng = np.random.default_rng()
+    proposals, proposal_logpdf, sample_logpdf = trans_proposer.propose(samples, rng)
+    acceptance_probs = np.minimum(
+        np.exp(
+            unnormalized_log_posterior_fn(proposals)
+            - unnormalized_log_posterior_fn(samples)
+            + sample_logpdf
+            - proposal_logpdf
+        ),
+        np.ones(len(samples)),
+    )
+    accept = rng.binomial(n=1, p=acceptance_probs).reshape(-1, 1)
+    indices_of_updated_samples = np.nonzero(accept.flatten())[0]
+    acceptance_prob = np.sum(accept) / len(accept)
+    new_samples = proposals[indices_of_updated_samples]
+    return new_samples, indices_of_updated_samples, acceptance_prob
