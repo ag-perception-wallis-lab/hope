90f0bd0925eab07c16808ac9a3904bfd4c41d332
Lars Reining <lars.reining@stud.tu-darmstadt.de>
2026-05-27 15:29:05 +0200

    Add logistic regression without lapses

--- hope/psychometric_functions.py
+++ hope/psychometric_functions.py
@@ -15,3 +15,15 @@ def logistic_regression_lapses(X, fct_params):
     s = expit(fct_params[:, 3:] @ X.T + b)
     p = a + (k - a) * s
     return p
+
+
+def logistic_regression(X, fct_params):
+    if X.size == 1 and X.ndim <= 1:
+        X = X.reshape((1, 1))
+    if X.ndim == 1:
+        X = np.expand_dims(X, axis=0)
+    if fct_params.ndim == 1:
+        fct_params = np.expand_dims(fct_params, axis=0)
+    b = fct_params[:, 0][:, np.newaxis]
+    s = expit(fct_params[:, 1:] @ X.T + b)
+    return s
